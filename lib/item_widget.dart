1a1b58d4e33c4a96410acb5921adba3acd6b8f45
Ayahviramempesonah <bayusatriapurnomo@gmail.com>
2024-02-07 01:36:30 +0700

    first commit

---
+++ lib/item_widget.dart
@@ -0,0 +1,143 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_grocery_ui/data.dart';
+import 'package:flutter_grocery_ui/product_detail.dart';
+
+import 'models/product.dart';
+
+class ItemWidget extends StatelessWidget {
+  const ItemWidget({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return ProductDetail(
+            product: product,
+          );
+        }));
+      },
+      child: Card(
+        elevation: 2,
+        shadowColor: Colors.green,
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          //mainAxisSize: MainAxisSize.min,
+          children: [
+            Hero(
+              tag: product.images[0],
+              child: SizedBox(
+                width: 150,
+                child: Image.network(product.images[0]),
+              ),
+            ),
+            const SizedBox(
+              height: 1,
+              width: 1,
+            ),
+            Text(
+              'USD ${product.price}',
+              style: const TextStyle(
+                color: Colors.green,
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Padding(
+              //padding: EdgeInsets.all(1),
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                product.title,
+                maxLines: 2,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const Divider(
+              height: 2,
+              color: Colors.grey,
+            ),
+            const Column(
+              mainAxisSize: MainAxisSize.min,
+              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //  mainAxisSize:MainAxisSize.max,
+              children: [
+                // SizedBox(
+                //  // height: 10,
+                // ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  //mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.add_shopping_cart,
+                          size: 20,
+                          color: Colors.green,
+                        ),
+                        SizedBox(
+                          width: 2,
+                        ),
+                        Text(
+                          "Beli",
+                          style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          
+                          Icons.remove_circle_outline,
+                          size: 18,
+                          color: Colors.green
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5),
+                          child: Text('0'),
+                        ),
+                        Icon(
+                          Icons.add_circle_outline,
+                          size: 18,
+                          color: Colors.green,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
